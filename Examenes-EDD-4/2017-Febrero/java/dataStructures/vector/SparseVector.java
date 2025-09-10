ab3b7c29f94565c22b2a98a5879c175626cf0fdd
Pacolias <pacomolinac2003@gmail.com>
2025-09-10 10:07:25 +0200

    Resolucion de la parte de Java de 2017-Febrero

---
+++ Examenes-EDD-4/2017-Febrero/java/dataStructures/vector/SparseVector.java
@@ -0,0 +1,165 @@
+/******************************************************************************
+ * Student's name: Paco Molina Cuenca
+ * Student's group: Doble Grado Matemáticas e Ingeniería Informática
+ * Data Structures. E.T.S.I. de Informática UMA.
+******************************************************************************/
+
+package dataStructures.vector;
+import dataStructures.list.*;
+import java.util.Iterator;
+
+public class SparseVector<T> implements Iterable<T> {
+
+    protected interface Tree<T> {
+
+        T get(int sz, int i);
+
+        Tree<T> set(int sz, int i, T x);
+    }
+
+    // Unif Implementation
+
+    protected static class Unif<T> implements Tree<T> {
+
+        private T elem;
+
+        public Unif(T e) {
+            elem = e;
+        }
+
+        @Override
+        public T get(int sz, int i) {
+            return elem;
+        }
+
+        @Override
+        public Tree<T> set(int sz, int i, T x) {
+            if(sz == 1)
+                return new Unif<>(x);
+            else if(i < sz/2)
+                return new Node<>(set(sz/2, i, x), new Unif<>(elem)).simplify();
+            else 
+                return new Node<>(new Unif<>(elem), set(sz/2, i - sz/2, x)).simplify();
+        }
+        
+        @Override
+        public String toString() {
+            return "Unif(" + elem + ")";
+        }
+    }
+
+    // Node Implementation
+
+    protected static class Node<T> implements Tree<T> {
+
+        private Tree<T> left, right;
+
+        public Node(Tree<T> l, Tree<T> r) {
+            left = l;
+            right = r;
+        }
+
+        @Override
+        public T get(int sz, int i) {
+            return i < sz/2 ? left.get(sz/2, i) : right.get(sz/2, i - sz/2);
+        }
+
+        @Override
+        public Tree<T> set(int sz, int i, T x) {
+            if(i < sz/2)
+                left = left.set(sz/2, i, x);
+            else 
+                right = right.set(sz/2, i - sz/2, x);
+            
+            this.simplify();
+
+            return this;
+        }
+
+        protected Tree<T> simplify() {
+            if(left instanceof Unif<?> && right instanceof Unif<?>){
+                Unif<T> leftLeaf = (Unif<T>) left;
+                Unif<T> rightLeaf = (Unif<T>) right;
+
+                return leftLeaf.elem.equals(rightLeaf.elem) ? new Unif<>(leftLeaf.elem) : this;
+            }
+
+            return this;
+        }
+
+        @Override
+        public String toString() {
+            return "Node(" + left + ", " + right + ")";
+        }
+    }
+
+    // SparseVector Implementation
+
+    private int size;
+    private Tree<T> root;
+
+    public SparseVector(int n, T elem) {
+        if(n < 0)
+            throw new VectorException("SparseVector on non positive integer");
+
+        size = (int) Math.pow(2, n);
+        root = new Unif<>(elem);
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public T get(int i) {
+        if(i < 0 || i >= size) 
+            throw new VectorException("Index out of bounds");
+
+        return root.get(size, i);
+    }
+
+    public void set(int i, T x) {
+        if(i < 0 || i >= size) 
+            throw new VectorException("Index out of bounds");
+
+        root = root.set(size, i, x);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        List<T> aux = new LinkedList<>();
+
+        for(int i = 0; i < size; i++)
+            aux.append(get(i));
+
+        return aux.iterator();
+    }
+
+    @Override
+    public String toString() {
+        return "SparseVector(" + size + "," + root + ")";
+    }
+
+    public SparseVector(SparseVector<T> sp){
+        this.size = sp.size();
+        root = new Unif<>(sp.get(0));
+
+        Iterator<T> itSp = sp.iterator();
+        int i = 0;
+
+        while(itSp.hasNext()){
+            this.set(i, itSp.next());
+            i++;
+        }
+    }
+
+    public int depthOf(int i){
+        return depthOfRec(i, size, 0, root);
+    }
+
+    private int depthOfRec(int i, int size, int depth, Tree<T> node){
+        if(node instanceof Unif<?>)
+            return depth;
+        
+        return i < size/2 ? depthOfRec(i, size/2, depth + 1, node.left) : depthOfRec(i - size/2, size/2, depth + 1, node.right);
+    }
+}
