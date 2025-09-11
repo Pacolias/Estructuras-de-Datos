b343f74fc84cab4b9d5ef36fed7a7e54ab579f63
Pacolias <pacomolinac2003@gmail.com>
2025-09-12 01:17:01 +0200

    Resolucion de la parte de Haskell de 2016-Febrero

---
+++ Examenes-EDD-4/2016-Febrero/Haskell/DataStructures/Graph/EulerianCycle.hs
@@ -0,0 +1,59 @@
+-------------------------------------------------------------------------------
+-- Student's name: Paco Molina Cuenca
+-- Student's group: Doble Grado Matemáticas e Ingeniería Informática
+--
+-- Data Structures. Escuela Térnica Superior de Ingenieria Informática. UMA.
+-------------------------------------------------------------------------------
+
+module DataStructures.Graph.EulerianCycle(isEulerian, remove, extractCycle, connectCycles, vertexInCommon, eulerianCycle) where
+
+import DataStructures.Graph.Graph
+import Data.List
+
+--H.1)
+isEulerian :: Eq a => Graph a -> Bool
+isEulerian g = and [even (degree g v) | v <- vertices g]
+
+-- H.2)
+remove :: (Eq a) => Graph a -> (a,a) -> Graph a
+remove g edge = mkGraphEdges [v | v <- vertices g', degree g' v /= 0] (edges g')
+    where 
+        g' = deleteEdge g edge
+
+-- H.3)
+extractCycle :: (Eq a) => Graph a -> a -> (Graph a, Path a)
+extractCycle g v0 = extractCycleRec g v0 [v0]
+    where 
+        extractCycleRec g v ciclo
+            | v == v0 && length ciclo > 1 = (g, ciclo)
+            | otherwise                   = extractCycleRec (remove g (v,u)) u (ciclo ++ [u]) 
+                where 
+                    u = head (successors g v)
+   
+-- H.4)
+connectCycles :: (Eq a) => Path a -> Path a -> Path a
+connectCycles [] ys = ys 
+connectCycles (x:xs) (y:ys)
+    | x == y    = x : ys ++ xs 
+    | otherwise = x : connectCycles xs (y:ys)
+
+-- H.5)
+vertexInCommon :: Eq a => Graph a -> Path a -> a
+vertexInCommon g xs = head [v | v <- vertices g, v `elem` xs]
+
+-- H.6) 
+eulerianCycle :: Eq a => Graph a -> Path a
+eulerianCycle g 
+    | not(isEulerian g) = error "eulerianCycle on not eulerian graph"
+    | otherwise         = eulerianCycleRec g [] (head (vertices g))
+        where 
+            eulerianCycleRec g xs v
+                | isEmpty g = xs 
+                | otherwise = eulerianCycleRec g' (connectCycles xs ys) u
+                    where 
+                        (g', ys) = extractCycle g v 
+                        u        = vertexInCommon g' ys 
+
+
+
+
