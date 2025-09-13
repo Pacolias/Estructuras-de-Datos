53d7d8006399ad51c94b5000736550490d11a462
Pacolias <pacomolinac2003@gmail.com>
2025-09-13 17:43:48 +0200

    Resolucion de la parte de Haskell de 2019-Febrero

---
+++ Examenes-EDD-4/2019-Febrero/Haskell/Kruskal.hs
@@ -0,0 +1,46 @@
+----------------------------------------------
+-- Estructuras de Datos.  2018/19
+-- 2º Curso del Grado en Ingeniería [Informática | del Software | de Computadores].
+-- Escuela Técnica Superior de Ingeniería en Informática. UMA
+--
+-- Examen 4 de febrero de 2019
+--
+-- ALUMNO/NAME: Paco Molina Cuenca
+-- GRADO/STUDIES: Doble Grado Matemáticas e Ingeniería Infomática
+-- NÚM. MÁQUINA/MACHINE NUMBER:
+--
+----------------------------------------------
+
+module Kruskal(kruskal, kruskals) where
+
+import qualified DataStructures.Dictionary.AVLDictionary as D
+import qualified DataStructures.PriorityQueue.LinearPriorityQueue as Q
+import DataStructures.Graph.DictionaryWeightedGraph
+
+kruskal :: (Ord a, Ord w) => WeightedGraph a w -> [WeightedEdge a w]
+kruskal wg = kruskalRec (foldr (\v -> D.insert v v) D.empty (vertices wg)) (foldr Q.enqueue Q.empty (edges wg)) []
+    where 
+        kruskalRec dict pq t 
+            | Q.isEmpty pq = t
+            | otherwise    = 
+                if repOrg /= repDst 
+                    then kruskalRec (D.updateOrInsert repDst (const org) org dict) (Q.dequeue pq) (firstEdge : t) 
+                else 
+                    kruskalRec dict (Q.dequeue pq) t
+
+                where 
+                    firstEdge@(WE org w dst) = Q.first pq 
+
+                    repOrg = representante org 
+                    repDst = representante dst 
+
+                    representante v = 
+                        case D.valueOf v dict of 
+                            Nothing -> error "error"
+                            Just u  -> if u == v then v else representante u
+
+-- Solo para evaluación continua / only for part time students
+kruskals :: (Ord a, Ord w) => WeightedGraph a w -> [[WeightedEdge a w]]
+kruskals = undefined
+-- se hace igual que kruskal solo que añadir en el caso en que es el mismo su propio representante 
+-- (creo) y cambiar la salida para que sea lista de listas de aristas 
