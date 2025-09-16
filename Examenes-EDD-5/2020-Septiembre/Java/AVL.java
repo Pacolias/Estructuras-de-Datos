e2e67cd7db696029b8803959e7b01b0c93d39dde
Pacolias <pacomolinac2003@gmail.com>
2025-09-17 01:53:01 +0200

    Resolucion de la parte de Java de 2020-Septiembre

--- Examenes-EDD-5/2020-Septiembre/Java/AVL.java
+++ Examenes-EDD-5/2020-Septiembre/Java/AVL.java
@@ -3,10 +3,8 @@
  * Student's group: Doble Grado Matemáticas e Ingeniería Informática
  */
 
-import dataStructures.list.List;
 import dataStructures.list.LinkedList;
-
-import java.util.Iterator;
+import dataStructures.list.List;
 
 
 class Bin {
@@ -14,20 +12,26 @@ class Bin {
     private List<Integer> weights; // weights of objects included in this bin
 
     public Bin(int initialCapacity) {
+        remainingCapacity = initialCapacity;
+        weights = new LinkedList<>();
     }
 
     // returns capacity left for this bin
     public int remainingCapacity() {
+        return remainingCapacity;
     }
 
     // adds a new object to this bin
     public void addObject(int weight) {
+        if(remainingCapacity < weight)
+            throw new IllegalArgumentException("addObject on bin without enough capacity");
+        
+        remainingCapacity -= weight;
+        weights.append(weight);
     }
 
     // returns an iterable through weights of objects included in this bin
     public Iterable<Integer> objects() {
-        //  SOLO PARA ALUMNOS SIN EVALUACIÓN CONTINUA
-        //  ONLY FOR STUDENTS WITHOUT CONTINUOUS ASSESSMENT
         return weights;
     }
 
@@ -53,50 +57,131 @@ public class AVL {
 
         // recomputes height of this node
         void setHeight() {
+            height = 1 + Math.max(height(left), height(right));
         }
 
         // recomputes max capacity among bins in tree rooted at this node
         void setMaxRemainingCapacity() {
+            maxRemainingCapacity = Math.max(Math.max(bin.remainingCapacity(), maxRemainingCapacity(left)), maxRemainingCapacity(right));
         }
 
         // left-rotates this node. Returns root of resulting rotated tree
         Node rotateLeft() {
+            Node node = new Node();
+            
+            Node leftChild = new Node();
+            leftChild.bin = bin;
+            leftChild.left = left;
+            leftChild.right = right.left;
+
+            node.bin = right.bin;
+            node.left = leftChild;
+            node.right = right.right;
+
+            return node;
         }
     }
     
     private static int maxRemainingCapacity(Node node) {
+        return node == null ? 0 : node.maxRemainingCapacity;
     }
 
     private static int height(Node node) {
+        return node == null ? 0 : node.height;
     }
 
     private Node root; // root of AVL tree
 
     public AVL() {
+        root = null;
     }
 
     // adds a new bin at the end of right spine.
     private void addNewBin(Bin bin) {
+        root = addNewBinRec(bin, root);
     }
 
-    private void addNewBinRec(Bin bin, Node root) {
+    private Node addNewBinRec(Bin bin, Node root) {
+        if(root == null){
+            root = new Node();
+            root.bin = bin;
+
+        } else{
+            root.right = addNewBinRec(bin, root.right);
+
+            if(height(root.right) > height(root.left) + 1)
+                root.rotateLeft();
+
+            root.setHeight();
+            root.setMaxRemainingCapacity();
+        }
+
+        return root;
     }
 
     // adds an object to first suitable bin. Adds
     // a new bin if object cannot be inserted in any existing bin
     public void addFirst(int initialCapacity, int weight) {
+        root = addFirstRec(initialCapacity, weight, root);
     }
 
-    private void addFirstRec(int initialCapacity, int weight, Node root) {
+    private Node addFirstRec(int initialCapacity, int weight, Node root) {
+        if(root == null){
+            root = new Node();
+            root.bin = new Bin(initialCapacity);
+            root.bin.addObject(weight);
+
+            root.setHeight();
+            root.setMaxRemainingCapacity();
+
+        } else if(maxRemainingCapacity(root) < weight){
+            root.right = addFirstRec(initialCapacity, weight, root.right);
+            root.setHeight();
+            root.setMaxRemainingCapacity();
+
+        } else if(maxRemainingCapacity(root.left) >= weight){
+            root.left = addFirstRec(initialCapacity, weight, root.left);
+            root.setHeight();
+            root.setMaxRemainingCapacity();
+
+        } else if(root.bin.remainingCapacity() >= weight){
+            root.bin.addObject(weight);
+
+        } else{
+            root.right = addFirstRec(initialCapacity, weight, root.right);
+            root.setHeight();
+            root.setMaxRemainingCapacity();
+        }
+
+        return root;
     }
 
     public void addAll(int initialCapacity, int[] weights) {
+        for(Integer w : weights)
+            addFirst(initialCapacity, w);
     }
 
     public List<Bin> toList() {
+        return toListRec(root);
     }
 
     private List<Bin> toListRec(Node root) {
+        List<Bin> res = new LinkedList<>();
+
+        if(root != null){
+            List<Bin> leftList = toListRec(root.left);
+            List<Bin> rightList = toListRec(root.right);
+
+            for(Bin bin : leftList)
+                res.append(bin);
+
+            res.append(root.bin);
+
+            for(Bin bin : rightList)
+                res.append(bin);
+        }
+
+        return res;
     }
 
     public String toString() {
@@ -130,16 +215,41 @@ public class AVL {
 
 class LinearBinPacking {
     public static List<Bin> linearBinPacking(int initialCapacity, List<Integer> weights) {
-        // todo
-        //  SOLO PARA ALUMNOS SIN EVALUACION CONTINUA
-        //  ONLY FOR STUDENTS WITHOUT CONTINUOUS ASSESSMENT
-        return null;
+        List<Bin> res = new LinkedList<>();
+
+        for(Integer w : weights)
+            insert(res, w, initialCapacity);
+
+        return res;
+    }
+
+    private static void insert(List<Bin> res, Integer w, int initialCapacity){
+        int i = 0;
+        boolean insertado = false;
+
+        while(i < res.size() && !insertado){
+            if(res.get(i).remainingCapacity() >= w){
+                res.get(i).addObject(w);
+                insertado = true;
+            }
+            i++;
+        }
+
+        if(!insertado){
+            Bin bin = new Bin(initialCapacity);
+            bin.addObject(w);
+            res.append(bin);
+        }
     }
 
 	public static Iterable<Integer> allWeights(Iterable<Bin> bins) {
-        // todo
-        //  SOLO PARA ALUMNOS SIN EVALUACION CONTINUA
-        //  ONLY FOR STUDENTS WITHOUT CONTINUOUS ASSESSMENT
-        return null;
+        List<Integer> res = new LinkedList<>();
+
+        for(Bin bin : bins){
+            for(Integer w : bin.objects())
+                res.append(w);
+        }
+
+        return res;
 	}
 }
