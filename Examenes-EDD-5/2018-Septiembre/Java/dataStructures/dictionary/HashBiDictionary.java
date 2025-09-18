8152f7c24f3a5787f7818adcdbf0c10222d1e9eb
Pacolias <pacomolinac2003@gmail.com>
2025-09-18 02:33:44 +0200

    Resolucion de la parte de Java de 2018-Septiembre

--- Examenes-EDD-5/2018-Septiembre/Java/dataStructures/dictionary/HashBiDictionary.java
+++ Examenes-EDD-5/2018-Septiembre/Java/dataStructures/dictionary/HashBiDictionary.java
@@ -2,7 +2,9 @@ package dataStructures.dictionary;
 import dataStructures.list.List;
 
 import dataStructures.list.ArrayList;
+import dataStructures.list.LinkedList;
 import dataStructures.set.AVLSet;
+import dataStructures.set.HashSet;
 import dataStructures.set.Set;
 import dataStructures.tuple.Tuple2;
 
@@ -21,26 +23,38 @@ public class HashBiDictionary<K,V> implements BiDictionary<K,V>{
 	private Dictionary<V,K> bValues;
 	
 	public HashBiDictionary() {
-
+		bKeys = new HashDictionary<>();
+		bValues = new HashDictionary<>();
 	}
 	
 	public boolean isEmpty() {
-
+		return bKeys.isEmpty();
 	}
 	
 	public int size() {
-
+		return bKeys.size();
 	}
 	
 	public void insert(K k, V v) {
+		if(bKeys.isDefinedAt(k))
+			bValues.delete(bKeys.valueOf(k));
+	
+		if(bValues.isDefinedAt(v))
+			bKeys.delete(bValues.valueOf(v));
+
+		bKeys.delete(k);
+		bValues.delete(v);
+
+		bKeys.insert(k, v);
+		bValues.insert(v, k);
 	}
 	
 	public V valueOf(K k) {
-
+		return bKeys.valueOf(k);
 	}
 	
 	public K keyOf(V v) {
-
+		return bValues.valueOf(v);
 	}
 	
 	public boolean isDefinedKeyAt(K k) {
@@ -52,11 +66,17 @@ public class HashBiDictionary<K,V> implements BiDictionary<K,V>{
 	}
 	
 	public void deleteByKey(K k) {
-
+		if(bKeys.isDefinedAt(k)){
+			bValues.delete(bKeys.valueOf(k));
+			bKeys.delete(k);
+		}
 	}
 	
 	public void deleteByValue(V v) {
-
+		if(bValues.isDefinedAt(v)){
+			bKeys.delete(bValues.valueOf(v));
+			bValues.delete(v);
+		}
 	}
 	
 	public Iterable<K> keys() {
@@ -73,28 +93,86 @@ public class HashBiDictionary<K,V> implements BiDictionary<K,V>{
 	
 		
 	public static <K,V extends Comparable<? super V>> BiDictionary<K, V> toBiDictionary(Dictionary<K,V> dict) {
+		if(!isInjective(dict))
+			throw new IllegalArgumentException("no inyectivo");
 
+		BiDictionary<K,V> res = new HashBiDictionary<>();
+
+		for(Tuple2<K,V> par : dict.keysValues())
+			res.insert(par._1(), par._2());
+
+		return res;
+	}
+
+	private static <K,V extends Comparable<? super V>> boolean isInjective(Dictionary<K,V> dict){
+		Set<K> keys = new HashSet<>();
+		Set<V> values = new HashSet<>();
+
+		for(K k : dict.keys())
+			keys.insert(k);
+
+		for(V v : dict.values())
+			values.insert(v);
+
+		return keys.size() == values.size();
 	}
 	
 	public <W> BiDictionary<K, W> compose(BiDictionary<V,W> bdic) {
+		BiDictionary<K,W> res = new HashBiDictionary<>();
 
+		for(K a : bKeys.keys()){
+			if(bdic.isDefinedKeyAt(bKeys.valueOf(a)))
+				res.insert(a, bdic.valueOf(bKeys.valueOf(a)));
+		}
+
+		return res;
 	}
 		
 	public static <K extends Comparable<? super K>> boolean isPermutation(BiDictionary<K,K> bd) {
+		Set<K> keys = new HashSet<>();
+		Set<K> values = new HashSet<>();
 
+		for(K k : bd.keys())
+			keys.insert(k);
+
+		for(K k : bd.values())
+			values.insert(k);
+
+		return keys.equals(values);
 	}
 	
 	// Solo alumnos con evaluación por examen final.
     // =====================================
 	
 	public static <K extends Comparable<? super K>> List<K> orbitOf(K k, BiDictionary<K,K> bd) {
-		// TODO
-		return null;
+		List<K> res = new LinkedList<>();
+		K x = k;
+
+		while(!(x.equals(k) && res.size() > 0)){
+			res.append(x);
+			x = bd.valueOf(x);
+		}
+
+		return res;
 	}
 	
 	public static <K extends Comparable<? super K>> List<List<K>> cyclesOf(BiDictionary<K,K> bd) {
-		// TODO
-		return null;
+		Set<K> keys = new HashSet<>();
+		List<List<K>> res = new LinkedList<>();
+		List<K> ciclo = new LinkedList<>();
+
+		for(K k : bd.keys()){
+			if(!keys.isElem(k)){
+				ciclo = orbitOf(k, bd);
+
+				for(K key : ciclo)
+					keys.insert(key);
+
+				res.append(ciclo);
+			}
+		}
+
+		return res;
 	}
 
     // =====================================
