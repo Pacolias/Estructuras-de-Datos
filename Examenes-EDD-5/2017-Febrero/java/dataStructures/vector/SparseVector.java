a2e812f71169333bfd51f552bb045580c4c6e8ba
Paco Molina <pacomolinac2003@gmail.com>
2025-09-19 17:22:44 +0200

    Resolucion parcial de la parte de Java de 2017-Febrero

--- Examenes-EDD-5/2017-Febrero/java/dataStructures/vector/SparseVector.java
+++ Examenes-EDD-5/2017-Febrero/java/dataStructures/vector/SparseVector.java
@@ -6,8 +6,8 @@
 
 package dataStructures.vector;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Iterator;
+import java.util.List;
 
 public class SparseVector<T> implements Iterable<T> {
 
@@ -30,10 +30,17 @@ public class SparseVector<T> implements Iterable<T> {
 
         @Override
         public T get(int sz, int i) {
+            return elem;
         }
 
         @Override
         public Tree<T> set(int sz, int i, T x) {
+            if(sz == 1)
+                return new Unif<>(x);
+            else if(i < sz/2)
+                return new Node<>(set(sz/2, i, x), new Unif<>(elem)).simplify();
+            else 
+                return new Node<>(new Unif<>(elem), set(sz/2, i - sz/2, x)).simplify();
         }
         
         @Override
@@ -55,13 +62,23 @@ public class SparseVector<T> implements Iterable<T> {
 
         @Override
         public T get(int sz, int i) {
+            return i < sz/2 ? left.get(sz/2, i) : right.get(sz/2, i - sz/2);
         }
 
         @Override
         public Tree<T> set(int sz, int i, T x) {
+            return i < sz/2 ? new Node<>(left.set(sz/2, i, x), right).simplify() : new Node<>(left, right.set(sz/2, i - sz/2, x));
         }
 
         protected Tree<T> simplify() {
+            if(left instanceof Unif<?> && right instanceof Unif<?>){
+                Unif<T> leftLeaf = (Unif<T>) left;
+                Unif<T> rightLeaf = (Unif<T>) right;
+
+                return leftLeaf.elem.equals(rightLeaf.elem) ? new Unif<>(leftLeaf.elem) : this;
+            }
+
+            return this;
         }
 
         @Override
@@ -76,19 +93,33 @@ public class SparseVector<T> implements Iterable<T> {
     private Tree<T> root;
 
     public SparseVector(int n, T elem) {
+        if(n < 0)
+            throw new VectorException("non positive integer");
+
+        size = (int) Math.pow(2, n);
+        root = new Unif<>(elem);
     }
 
     public int size() {
+        return size;
     }
 
     public T get(int i) {
+        return root.get(size, i);
     }
 
     public void set(int i, T x) {
+        root = root.set(size, i, x);
     }
 
     @Override
     public Iterator<T> iterator() {
+        List<T> aux = new ArrayList<>();
+
+        for(int i = 0; i < size; i++)
+            aux.add(get(i));
+
+        return aux.iterator();
     }
 
     @Override
