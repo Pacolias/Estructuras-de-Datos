dcf1d173e315139f9ecf0f605d6f14ed39fdd3da
Pacolias <pacomolinac2003@gmail.com>
2025-09-19 02:00:35 +0200

    Resolucion de la parte de Haskell de 2018-Febrero

---
+++ Examenes-EDD-5/2018-Febrero/Haskell/DisjointSet.hs
@@ -0,0 +1,285 @@
+-------------------------------------------------------------------------------
+-- Student's name: Paco Molina Cuenca
+-- Student's group:
+--
+-- Data Structures. February 2018. BSc. Computer Science. UMA.
+-------------------------------------------------------------------------------
+
+module DataStructures.Set.DisjointSet
+                  ( DisjointSet
+                  , empty
+                  , isEmpty
+                  , isElem
+                  , numElements
+                  , add
+                  , areConnected
+                  , kind
+                  , union
+                  , flatten
+                  , kinds
+                  ) where
+
+import           Data.List                               (intercalate, (\\))
+import           Data.Maybe                              (fromJust)
+import qualified DataStructures.Dictionary.AVLDictionary as D
+
+data DisjointSet a = DS (D.Dictionary a a)
+
+-- | Exercise 1. empty
+
+empty :: DisjointSet a
+empty = DS D.empty
+
+-- | Exercise 2.a isEmpty
+
+isEmpty :: DisjointSet a -> Bool
+isEmpty (DS dict) = D.isEmpty dict
+
+-- | Exercise 2.b isElem
+
+isElem :: (Ord a) => a -> DisjointSet a -> Bool
+isElem x (DS dict) = D.isDefinedAt x dict
+
+-- | Exercise 3. numElements
+
+numElements :: DisjointSet a -> Int
+numElements (DS dict) = D.size dict 
+
+-- | Exercise 4. add
+
+add :: Ord a => a -> DisjointSet a -> DisjointSet a
+add x ds@(DS dict)
+  | isElem x ds = ds 
+  | otherwise   = DS (D.insert x x dict)
+
+-- | Exercise 5. root
+
+root :: Ord a => a -> DisjointSet a -> Maybe a
+root x ds@(DS dict) = 
+  case D.valueOf x dict of 
+    Nothing -> Nothing
+    Just r  -> if x == r then Just r else root r ds
+
+-- | Exercise 6. isRoot
+
+isRoot :: Ord a => a -> DisjointSet a -> Bool
+isRoot x ds = Just x == root x ds
+
+-- | Exercise 7. areConnected
+
+areConnected :: Ord a => a -> a -> DisjointSet a -> Bool
+areConnected x y ds = root x ds == root y ds
+-- | Exercise 8. kind
+
+kind :: Ord a => a -> DisjointSet a -> [a]
+kind x ds@(DS dict) = [y | y <- D.keys dict, areConnected x y ds]
+
+-- | Exercise 9. union
+
+union :: Ord a => a -> a -> DisjointSet a -> DisjointSet a
+union x y ds@(DS dict)
+  | rootX < rootY   = DS (D.insert rootY rootX dict)
+  | rootX == rootY  = ds
+  | otherwise       = DS (D.insert rootX rootY dict)
+  where 
+    rootX =
+      case root x ds of 
+        Nothing -> error ""
+        Just r  -> r 
+
+    rootY =
+      case root y ds of 
+        Nothing -> error ""
+        Just r  -> r 
+
+-- |------------------------------------------------------------------------
+
+flatten :: Ord a => DisjointSet a -> DisjointSet a
+flatten ds@(DS dict) = DS (flattenRec (D.keys dict))
+  where 
+    flattenRec []     = D.empty 
+    flattenRec (k:ks) = 
+      case root k ds of 
+        Nothing -> flattenRec ks 
+        Just r  -> D.insert k r (flattenRec ks)
+
+kinds :: Ord a => DisjointSet a -> [[a]]
+kinds ds@(DS dict) = kindsRec (D.keys dict)
+  where 
+    kindsRec []     = []
+    kindsRec (k:ks) = claseEq : kindsRec (ks \\ claseEq)
+      where 
+        claseEq = kind k ds  
+
+-- |------------------------------------------------------------------------
+
+instance (Ord a, Show a) => Show (DisjointSet a) where
+  show (DS d)  = "DictionaryDisjointSet(" ++ intercalate "," (map show (D.keysValues d)) ++ ")"
+
+
+{-
+
+-- Examples
+
+-- | Exercise 1. empty
+
+>>> empty
+DictionaryDisjointSet()
+
+-- | Exercise 2.a isEmpty
+
+>>> isEmpty empty
+True
+
+>>> isEmpty (add 1 empty)
+False
+
+-- | Exercise 2.b isElem
+
+>>> isElem 1 empty
+False
+
+>>> isElem 1 (add 1 empty)
+True
+
+>>> isElem 2 (add 1 empty)
+False
+
+>>> isElem 1 (add 2 (add 1 empty))
+True
+
+-- | Exercise 3. numElements
+
+>>> numElements empty
+0
+
+>>> numElements (add 1 empty)
+1
+
+>>> numElements (add 2 (add 1 empty))
+2
+
+-- | Exercise 4. add
+
+>>> add 1 empty
+DictionaryDisjointSet((1,1))
+
+>>> add 2 (add 1 empty)
+DictionaryDisjointSet((1,1),(2,2))
+
+>>> add 1 (add 2 (add 1 empty))
+DictionaryDisjointSet((1,1),(2,2))
+
+-- | Exercise 5. root
+
+>>> root 1 empty
+Nothing
+
+>>> root 1 (add 1 empty)
+Just 1
+
+>>> root 2 (add 2 (add 1 empty))
+Just 2
+
+>>> root 1 (union 1 2 (add 2 (add 1 empty)))
+Just 1
+
+>>> root 2 (union 1 2 (add 2 (add 1 empty)))
+Just 1
+
+>>> root 1 (union 1 3 (add 3 (add 2 (add 1 empty))))
+Just 1
+
+>>> root 2 (union 1 3 (add 3 (add 2 (add 1 empty))))
+Just 2
+
+>>> root 3 (union 1 3 (add 3 (add 2 (add 1 empty))))
+Just 1
+
+>>> root 4 (union 1 3 (add 3 (add 2 (add 1 DisjointSet.empty))))
+Nothing
+
+-- | Exercise 6. isRoot
+
+>>> isRoot 1 empty
+False
+
+>>> isRoot 1 (add 1 empty)
+True
+
+>>> isRoot 1 (union 1 2 (add 2 (add 1 empty)))
+True
+
+>>> isRoot 2 (union 1 2 (add 2 (add 1 empty)))
+False
+
+>>> isRoot 1 (union 1 3 (add 3 (add 2 (add 1 empty))))
+True
+
+>>> isRoot 2 (union 1 3 (add 3 (add 2 (add 1 empty))))
+True
+
+>>> isRoot 3 (union 1 3 (add 3 (add 2 (add 1 empty))))
+False
+
+-- | Exercise 7. areConnected
+
+>>> areConnected 1 3 (union 1 3 (add 3 (add 2 (add 1 empty))))
+True
+
+>>> areConnected 3 1 (union 1 3 (add 3 (add 2 (add 1 empty))))
+True
+
+>>> areConnected 1 1 (union 1 3 (add 3 (add 2 (add 1 empty))))
+True
+
+>>> areConnected 1 2 (union 1 3 (add 3 (add 2 (add 1 empty))))
+False
+
+>>> areConnected 1 2 (union 2 3 (union 1 3 (add 3 (add 2 (add 1 empty)))))
+True
+
+>>> areConnected 1 5 (union 2 3 (union 1 3 (add 3 (add 2 (add 1 empty)))))
+False
+
+-- | Exercise 8. kind
+
+>>> kind 1 (add 2 (add 1 empty))
+[1]
+
+>>> kind 2 (add 2 (add 1 empty))
+[2]
+
+>>> kind 3 (add 2 (add 1 empty))
+[]
+
+>>> kind 1 (union 1 3 (add 3 (add 2 (add 1 empty))))
+[1,3]
+
+>>> kind 3 (union 1 3 (add 3 (add 2 (add 1 empty))))
+[1,3]
+
+>>> kind 2 (union 1 3 (add 3 (add 2 (add 1 empty))))
+[2]
+
+>>> kind 2 (union 2 3 (union 1 3 (add 3 (add 2 (add 1 empty)))))
+[1,2,3]
+
+-- | Exercise 9. union
+
+>>> union 1 2 (add 2 (add 1 empty))
+DictionaryDisjointSet((1,1),(2,1))
+
+>>> union 2 1 (add 2 (add 1 empty))
+DictionaryDisjointSet((1,1),(2,1))
+
+>>> union 1 1 (add 2 (add 1 empty))
+DictionaryDisjointSet((1,1),(2,2))
+
+>>> union 1 3 (add 3 (add 2 (add 1 empty)))
+DictionaryDisjointSet((1,1),(2,2),(3,1))
+
+>>> union 1 2 (add 1 empty)
+*** Exception: union: missing element(s)
+
+-}
---
+++ Examenes-EDD-5/2018-Febrero/Java/dataStructures/set/DisjointSetDictionary.java
@@ -0,0 +1,126 @@
+/**
+ * Estructuras de Datos. Grados en Informatica. UMA.
+ * Examen de febrero de 2018.
+ * Apellidos, Nombre: Paco Molina Cuenca
+ * Titulacion, Grupo: Doble Grado Matemáticas e Ingeniería Informática.
+ */
+
+package dataStructures.set;
+
+import dataStructures.dictionary.AVLDictionary;
+import dataStructures.dictionary.Dictionary;
+import dataStructures.list.ArrayList;
+import dataStructures.list.LinkedList;
+import dataStructures.list.List;
+
+public class DisjointSetDictionary<T extends Comparable<? super T>> implements DisjointSet<T> {
+
+    private Dictionary<T, T> dic;
+
+    /**
+     * Inicializa las estructuras necesarias.
+     */
+    public DisjointSetDictionary() {
+    }
+
+    /**
+     * Devuelve {@code true} si el conjunto no contiene elementos.
+     */
+    @Override
+    public boolean isEmpty() {
+    }
+
+    /**
+     * Devuelve {@code true} si {@code elem} es un elemento del conjunto.
+     */
+    @Override
+    public boolean isElem(T elem) {
+    }
+
+    /**
+     * Devuelve el numero total de elementos del conjunto.
+     */
+
+    @Override
+    public int numElements() {
+    }
+
+    /**
+     * Agrega {@code elem} al conjunto. Si {@code elem} no pertenece al
+     * conjunto, crea una nueva clase de equivalencia con {@code elem}. Si
+     * {@code elem} pertencece al conjunto no hace nada.
+     */
+    @Override
+    public void add(T elem) {
+    }
+
+    /**
+     * Devuelve el elemento canonico (la raiz) de la clase de equivalencia la
+     * que pertenece {@code elem}. Si {@code elem} no pertenece al conjunto
+     * devuelve {@code null}.
+     */
+    private T root(T elem) {
+    }
+
+    /**
+     * Devuelve {@code true} si {@code elem} es el elemento canonico (la raiz)
+     * de la clase de equivalencia a la que pertenece.
+     */
+    private boolean isRoot(T elem) {
+    }
+
+    /**
+     * Devuelve {@code true} si {@code elem1} y {@code elem2} estan en la misma
+     * clase de equivalencia.
+     */
+    @Override
+    public boolean areConnected(T elem1, T elem2) {
+    }
+
+    /**
+     * Devuelve una lista con los elementos pertenecientes a la clase de
+     * equivalencia en la que esta {@code elem}. Si {@code elem} no pertenece al
+     * conjunto devuelve la lista vacia.
+     */
+    @Override
+    public List<T> kind(T elem) {
+    }
+
+    /**
+     * Une las clases de equivalencias de {@code elem1} y {@code elem2}. Si
+     * alguno de los dos argumentos no esta en el conjunto lanzara una excepcion
+     * {@code IllegalArgumenException}.
+     */
+    @Override
+    public void union(T elem1, T elem2) {
+    }
+
+    // ====================================================
+    // A partir de aqui solo para alumnos a tiempo parcial
+    // que no sigan el proceso de evaluacion continua.
+    // ====================================================
+
+    /**
+     * Aplana la estructura de manera que todos los elementos se asocien
+     * directamente con su representante canonico.
+     */
+    @Override
+    public void flatten() {
+    }
+
+    /**
+     * Devuelve una lista que contiene las clases de equivalencia del conjunto
+     * como listas.
+     */
+    @Override
+    public List<List<T>> kinds() {
+    }
+
+    /**
+     * Devuelve una representacion del conjunto como una {@code String}.
+     */
+    @Override
+    public String toString() {
+        return "DisjointSetDictionary(" + dic.toString() + ")";
+    }
+}
