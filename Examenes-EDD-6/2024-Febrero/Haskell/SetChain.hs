03353fee018bba8b385edf4e20b5905fdaac5055
Paco Molina <pacomolinac2003@gmail.com>
2025-09-24 11:45:07 +0200

    Resolucion de la parte de Haskell de 2024-Febrero

---
+++ Examenes-EDD-6/2024-Febrero/Haskell/SetChain.hs
@@ -0,0 +1,91 @@
+-- Student's name: Paco Molina Cuenca
+-- Student's group: 
+-- Identity number (DNI if Spanish/passport if Erasmus): 
+ 
+module SetChain ( 
+    SetChain 
+    , empty 
+    , isEmpty 
+    , pendingTransactions 
+    , add 
+    , getEpoch 
+    , size 
+    , validate 
+    --  
+    , addAll     
+    , fold 
+    , toList 
+    ) where 
+ 
+import qualified DataStructures.Set.LinearSet as S 
+import qualified DataStructures.Dictionary.AVLDictionary as D 
+
+--                    Mempool            History                 Epoch 
+data SetChain a = SC (S.Set a) (D.Dictionary Integer (S.Set a)) Integer 
+ 
+-- ------------------------------- 
+-- DO NOT MODIFY THE CODE ABOVE 
+-- ------------------------------- 
+
+-- * Exercise a) 
+empty :: SetChain a
+empty = SC S.empty D.empty 0
+
+-- * Exercise b) 
+isEmpty :: SetChain a -> Bool
+isEmpty (SC mempool history epoch) = S.isEmpty mempool && D.isEmpty history && epoch == 0
+
+-- * Exercise c) 
+getEpoch :: (Eq a) => a -> SetChain a -> Integer 
+getEpoch trans (SC _ history _) = getEpochRec (D.keys history)
+  where 
+    getEpochRec []     = -1
+    getEpochRec (e:es) = 
+      case D.valueOf e history of 
+        Nothing       -> -1 
+        Just transSet -> if S.isElem trans transSet then e else getEpochRec es
+ 
+-- * Exercise d) 
+size :: SetChain a -> Int 
+size (SC _ history _) = sum [S.size transSet | transSet <- D.values history]
+ 
+-- * Exercise e) 
+pendingTransactions :: SetChain a -> Bool 
+pendingTransactions (SC mempool _ _) = not(S.isEmpty mempool)
+ 
+-- * Exercise f) 
+add :: (Eq a) => a -> SetChain a -> SetChain a 
+add trans (SC mempool history epoch) = SC (S.insert trans mempool) history epoch
+ 
+-- * Exercise g) 
+validate :: (Ord a) => SetChain a -> SetChain a 
+validate sc@(SC mempool history epoch)
+  | not(pendingTransactions sc)                        = sc 
+  | not(S.isEmpty (mempool `S.intersection` transSet)) = error "transaction already validated"
+  | otherwise                                          = SC S.empty (D.insert epoch mempool history) (epoch+1)
+  where 
+    transSet = foldr S.union S.empty (D.values history)
+ 
+-- * Exercise h) 
+addAll :: (Eq a) => [a] -> SetChain a -> SetChain a 
+addAll xs sc = foldr add sc xs
+ 
+-- * Exercise i) 
+fold :: (Ord a) => (a -> b -> b) -> b -> SetChain a -> b 
+fold f z (SC _ history _) = S.fold f z transSet
+  where 
+    transSet = foldr S.union S.empty (D.values history)
+        
+-- * Exercise j) 
+toList :: (Ord a) => SetChain a -> [a] 
+toList sc = fold (:) [] sc
+
+-- ------------------------------- 
+-- DO NOT MODIFY THE CODE BELOW 
+-- ------------------------------- 
+ 
+instance (Show a) => Show (SetChain a) where 
+  show (SC s h e) = concat ["SetChain(", show s, ", ", show h, ", ", show e, ")"] 
+ 
+instance (Eq a) => Eq (SetChain a) where 
+  (SC m1 h1 e1) == (SC m2 h2 e2) = m1 == m2 && h1 == h2 && e1 == e2
---
+++ Examenes-EDD-6/2024-Febrero/Haskell/TestSetChain.hs
@@ -0,0 +1,77 @@
+import Test.HUnit
+import Control.Exception (catch, evaluate, ErrorCall(..))
+import qualified DataStructures.Set.LinearSet as S
+import qualified DataStructures.Dictionary.AVLDictionary as D
+import SetChain
+
+-- Asumimos que el tipo SetChain y las funciones están importadas y definidas
+
+-- Un ejemplo sencillo de transacciones
+tx1, tx2, tx3 :: Int
+tx1 = 1
+tx2 = 2
+tx3 = 3
+
+-- Un SetChain vacío para comenzar
+emptySC :: SetChain Int
+emptySC = empty
+
+-- Test 1: empty y isEmpty
+testEmptyIsEmpty = TestCase $ do
+  assertBool "empty is empty" (isEmpty emptySC)
+  let sc1 = add tx1 emptySC
+  assertBool "after add, not empty" (not $ isEmpty sc1)
+
+-- Test 2: add y pendingTransactions
+testAddPending = TestCase $ do
+  let sc1 = add tx1 emptySC
+  assertBool "pending transactions after add" (pendingTransactions sc1)
+  let sc2 = validate sc1
+  assertBool "no pending transactions after validate" (not $ pendingTransactions sc2)
+
+-- Test 3: getEpoch
+testGetEpoch = TestCase $ do
+  let sc1 = add tx1 emptySC
+  assertEqual "getEpoch tx1 before validate" (-1) (getEpoch tx1 sc1)
+  let sc2 = validate sc1
+  assertEqual "getEpoch tx1 after validate" 0 (getEpoch tx1 sc2)
+
+-- Test 4: size
+testSize = TestCase $ do
+  let sc1 = add tx1 $ add tx2 emptySC
+  let sc2 = validate sc1
+  assertEqual "size after adds, before validate" 0 (size sc1)
+  assertEqual "size after validate" 2 (size sc2) -- ojo: size de validated set
+  let sc3 = add tx3 sc2
+  assertEqual "size after add post validate" 2 (size sc3)
+
+-- Test 5: fold y toList coherencia
+testFoldToList = TestCase $ do
+  let sc1 = addAll [tx1, tx2] emptySC
+  let sc2 = validate sc1
+  let lst = toList sc2
+  assertBool "toList contains tx1" (tx1 `elem` lst)
+  assertBool "toList contains tx2" (tx2 `elem` lst)
+  assertEqual "fold counts elements" (length lst) (fold (\_ acc -> acc + 1) 0 sc2)
+
+-- Test 6: validate error on duplicate
+testValidateDuplicate = TestCase $ do
+  let sc1 = addAll [tx1] emptySC
+  let sc2 = validate sc1
+  let sc3 = add tx1 sc2 -- trying to add duplicate tx1 again
+  result <- (evaluate (validate sc3) >> return True)
+            `catch` (\(ErrorCall _) -> return False)
+  assertBool "validate duplicate throws error" (not result)
+
+tests :: Test
+tests = TestList
+  [ TestLabel "Empty and isEmpty" testEmptyIsEmpty
+  , TestLabel "Add and pendingTransactions" testAddPending
+  , TestLabel "getEpoch" testGetEpoch
+  , TestLabel "Size function" testSize
+  , TestLabel "Fold and toList coherence" testFoldToList
+  , TestLabel "Validate throws on duplicate" testValidateDuplicate
+  ]
+
+main :: IO Counts
+main = runTestTT tests
