ccb84aebddc4909f651141adbed0056c3df000f6
Pacolias <pacomolinac2003@gmail.com>
2025-09-26 01:40:54 +0200

    Resolucion de la parte de Java de 2023-Febrero

--- Examenes-EDD-6/2023-Febrero/Java/dataStructures/trie/DictionaryStringTrie.java
+++ Examenes-EDD-6/2023-Febrero/Java/dataStructures/trie/DictionaryStringTrie.java
@@ -13,8 +13,6 @@ import dataStructures.dictionary.Dictionary;
 import dataStructures.list.LinkedList;
 import dataStructures.list.List;
 import dataStructures.tuple.Tuple2;
-
-import java.util.Iterator;
 import java.util.Objects;
 
 public class DictionaryStringTrie<V> {
@@ -35,50 +33,84 @@ public class DictionaryStringTrie<V> {
   ****************************************************************************/
 
   // | = Exercise a - constructor
-  public DictionaryStringTrie() {
-	  
+  public DictionaryStringTrie(){
+	  root = null;
   }
 
   // | = Exercise b - isEmpty
-  public boolean isEmpty() {
+  public boolean isEmpty(){
+    return root == null;
   }
   
 
   // | = Exercise c - sizeValue
-  protected static <V> int sizeValue(V value) {
-	  
+  protected static <V> int sizeValue(V value){
+	  return value == null ? 0 : 1;
   }
 
   // | = Exercise d - size
-  public int size() {
-	  
+  public int size(){
+    return size(root);
   }
 
-  protected static <V> int size(Node<V> n) {
+  protected static <V> int size(Node<V> n){
+    if(n == null)
+      return 0;
     
+    int size = sizeValue(n.value);
+
+    for(Node<V> node : n.children.values())
+      size += size(node);
+
+    return size;
   }
 
   // | = Exercise e - childOf
-  protected static <V> Node<V> childOf(char c, Node<V> node) {
-   
+  protected static <V> Node<V> childOf(char c, Node<V> node){
+    return node.children.isDefinedAt(c) ? node.children.valueOf(c) : null;
   }
 
   // | = Exercise f - search
-  public V search(String str) {
-
+  public V search(String str){
+    return search(str, root);
   }
 
-  protected static <V> V search(String str, Node<V> node) {
-    
+  protected static <V> V search(String str, Node<V> node){
+    if(node == null)
+      return null;
+    else if(str.isEmpty())
+      return node.value;
+    else 
+      return search(str.substring(1), childOf(str.charAt(0), node));
   }
 
   // | = Exercise g - insert
-  public void insert(String str, V value) {
-    
+  public void insert(String str, V value){
+    root = insert(str, value, root);
   }
 
-  protected static <V> Node<V> insert(String str, V value, Node<V> node) {
-    
+  protected static <V> Node<V> insert(String str, V value, Node<V> node){
+    if(str.isEmpty() && node == null){
+      Node<V> n = new Node<>();
+      n.value = value;
+      return n;
+
+    } else if(str.isEmpty()){
+      Node<V> n = new Node<>();
+      n.value = value;
+      n.children = node.children;
+
+      return n;
+
+    } else{
+
+      if(node == null)
+        node = new Node<>();
+
+      node.children.insert(str.charAt(0), insert(str.substring(1), value, childOf(str.charAt(0), node)));
+
+      return node;
+    }
   }
 
   /*****************************************************************************
@@ -87,19 +119,35 @@ public class DictionaryStringTrie<V> {
 
   // | = Exercise e1 - strings
   public List<String> strings() {
-    // TODO
-    return null;
+    return strings(root);
   }
 
   protected static <V> List<String> strings(Node<V> node) {
-    // TODO
-    return null;
+    List<String> res = new LinkedList<>();
+
+    if(node != null){
+      if(node.value != null)
+        res.append("");
+        
+      for(Tuple2<Character, Node<V>> par: node.children.keysValues()){
+        for(String str : strings(par._2()))
+          res.append(par._1() + str);
+      }
+    }
+
+    return res;
   }
 
   // | = Exercise e2 - fromList
   public static DictionaryStringTrie<Integer> fromList(List<String> list) {
-    // TODO
-    return null;
+    DictionaryStringTrie<Integer> res = new DictionaryStringTrie<>();
+
+    int i = 0;
+    for(String str : list){
+      res.insert(str, i);
+    }
+
+    return res;
   }
 
   /*****************************************************************************
