ffc5c2be891a8cbda2c643159865b2a0a7648db7
Paco Molina <pacomolinac2003@gmail.com>
2025-09-27 01:46:14 +0200

    Resolucion de la parte de Java de 2021-Septiembre

---
+++ Examenes-EDD-6/2021-Septiembre/Java/dataStructures/sparseMatrix/SparseMatrix.java
@@ -0,0 +1,86 @@
+package sparseMatrix;
+
+import java.util.List;
+import java.util.TreeMap;
+
+public class SparseMatrix {
+    private final int rows;
+    private final int cols;
+    private final TreeMap<Index, Integer> values;
+
+    public SparseMatrix(int rows, int cols){
+        this.rows = rows;
+        this.cols = cols;
+        values = new TreeMap<>();
+    }
+
+    // Devuelve el valor en una posición (privado)
+    private int value(Index idx){
+        return values.containsKey(idx) ? values.get(idx) : 0;
+    }
+
+    // Modifica el valor en una posición (privado)
+    private void update(Index idx, int val){
+        if(val == 0)
+            values.remove(idx);
+        else 
+            values.put(idx, val);
+    }
+
+    // Devuelve un índice validado
+    private Index index(int r, int c){
+        if(r < 0 || c < 0 || r >= rows || c >= cols)
+            throw new IllegalArgumentException();
+
+        return new Index(r, c);
+    }
+
+    // Pública: set
+    public void set(int r, int c, int val){
+        update(index(r, c), val);
+    }
+
+    // Pública: get
+    public int get(int r, int c){
+        return value(index(r, c));
+    }
+
+    // Pública: suma de dos matrices
+    public SparseMatrix add(SparseMatrix other){
+        if(rows != other.rows || cols != other.cols)
+            throw new IllegalArgumentException();
+
+        SparseMatrix res = new SparseMatrix(rows, cols);
+
+        for(Index idx : values.keySet())
+            res.set(idx.getRow(), idx.getColumn(), values.get(idx));
+
+        for(Index idx : other.values.keySet())
+            res.set(idx.getRow(), idx.getColumn(), other.get(idx.getRow(), idx.getColumn()) + res.get(idx.getRow(), idx.getColumn()));
+
+        return res;
+    }
+
+    // Pública: traspuesta
+    public SparseMatrix transpose(){
+        SparseMatrix res = new SparseMatrix(cols, rows);
+
+        for(Index idx : values.keySet())
+            res.set(idx.getColumn(), idx.getRow(), values.get(idx));
+
+        return res;
+    }
+
+    // Pública: fromList
+    public static SparseMatrix fromList(int rows, int cols, List<Integer> data){
+        if(data.size() % 3 != 0)
+            throw new IllegalArgumentException();
+
+        SparseMatrix res = new SparseMatrix(rows, cols);
+
+        for(int i = 0; i < data.size(); i += 3)
+            res.set(data.get(i), data.get(i + 1), data.get(i + 2));
+
+        return res;
+    }
+}
