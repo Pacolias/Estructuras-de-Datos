8b8f40dc8f6cfa775ccca43fbdfbaf5f9845fe06
Pacolias <pacomolinac2003@gmail.com>
2025-09-28 14:11:17 +0200

    Resolucion de la parte de Haskell de 2020-Febrero

---
+++ Examenes-EDD-6/2020-Febrero/Java/src/dataStructures/graph/FordFulkerson.java
@@ -0,0 +1,80 @@
+﻿/**
+ * Student's name: Paco Molina Cuenca
+ * Student's group: Doble Grado Matemáticas e Ingeniería Informática
+ */
+
+package dataStructures.graph;
+
+import dataStructures.list.LinkedList;
+import dataStructures.list.List;
+import dataStructures.set.HashSet;
+import dataStructures.set.Set;
+import dataStructures.tuple.Tuple2;
+
+public class FordFulkerson<V> {
+    private WeightedDiGraph<V,Integer> g; // Initial graph 
+    private List<WDiEdge<V,Integer>> sol; // List of edges representing maximal flow graph
+    private V src; 			  // Source
+    private V dst; 		  	  // Sink
+	
+    /**
+     * Constructors and methods
+     */
+
+    public static <V> int maxFlowPath(List<WDiEdge<V,Integer>> path) {
+
+    }
+
+    public static <V> List<WDiEdge<V,Integer>> updateEdge(V x, V y, Integer p, List<WDiEdge<V,Integer>> edges) {
+
+    }
+
+    public static <V> List<WDiEdge<V,Integer>> updateEdges(List<WDiEdge<V,Integer>> path, Integer p, List<WDiEdge<V,Integer>> edges) {
+
+    }
+
+    public static <V> List<WDiEdge<V,Integer>> addFlow(V x, V y, Integer p, List<WDiEdge<V,Integer>> sol) {
+    
+    }
+
+    public static <V> List<WDiEdge<V,Integer>> addFlows(List<WDiEdge<V,Integer>> path, Integer p, List<WDiEdge<V,Integer>> sol) {
+
+    }
+
+    public FordFulkerson(WeightedDiGraph<V,Integer> g, V src, V dst) {
+
+    }
+
+    public int maxFlow() {
+
+    }
+
+    public int maxFlowMinCut(Set<V> set) {
+
+    }
+
+    /**
+     * Provided auxiliary methods
+     */
+    public List<WDiEdge<V, Integer>> getSolution() {
+        return sol;
+    }
+	
+    /**********************************************************************************
+     * A partir de aquí SOLO para estudiantes a tiempo parcial sin evaluación continua.
+     * ONLY for part time students.
+     * ********************************************************************************/
+
+    public static <V> boolean localEquilibrium(WeightedDiGraph<V,Integer> g, V src, V dst) {
+        // TO DO
+        return false;
+    }
+    public static <V,W> Tuple2<List<V>,List<V>> sourcesAndSinks(WeightedDiGraph<V,W> g) {
+        // TO DO
+        return null;
+    }
+
+    public static <V> void unifySourceAndSink(WeightedDiGraph<V,Integer> g, V newSrc, V newDst) {
+        // TO DO
+    }
+}
