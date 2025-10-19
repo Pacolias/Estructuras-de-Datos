b9a7e724f92f9dfded6e109b1c05421138695f6b
Pacolias <pacomolinac2003@gmail.com>
2025-10-19 12:31:09 +0200

    Resolucion de la parte de Java de 2014-Febrero

---
+++ Examenes-EDD-7/2014-Febrero/Java/dataStructures/graph/SCCs.java
@@ -0,0 +1,59 @@
+// Examen Febrero 2014 - SCCs
+// Data Structures. UMA
+
+package dataStructures.graph;
+
+import static dataStructures.graph.DiGraphBFT.bft;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class SCCs {
+
+    // (A) Devuelve el grafo inverso
+    public static <V> DiGraph<V> reverseDiGraph(DiGraph<V> g) {
+        Set<DiEdge<V>> edges = new HashSet<>();
+
+        for(DiEdge<V> edge : g.edges())
+            edges.add(new DiEdge<>(edge.target(), edge.source()));
+
+        return DiGraph.fromEdges(g.vertices(), edges);
+    }
+
+    // (B) Subgrafo con los vértices dados
+    public static <V> DiGraph<V> restrictDiGraph(DiGraph<V> g, Set<V> vs) {
+        Set<DiEdge<V>> edges = new HashSet<>();
+
+        for(DiEdge<V> edge : g.edges()){
+            if(vs.contains(edge.source()) && vs.contains(edge.target()))
+                edges.add(edge);
+        }
+
+        return DiGraph.fromEdges(vs, edges);
+    }
+
+    // (C) SCC de un vértice
+    public static <V> Set<V> sccOf(DiGraph<V> g, V src) {
+        List<V> vs = bft(g, src);
+        DiGraph<V> gr = restrictDiGraph(g, new HashSet<>(vs));
+        DiGraph<V> g_prima = reverseDiGraph(gr);
+        return new HashSet<>(bft(g_prima, src));
+    }
+
+    // (D) Todas las componentes fuertemente conexas
+    public static <V> Set<Set<V>> stronglyConnectedComponentsDiGraph(DiGraph<V> g) {
+        Set<Set<V>> res = new HashSet<>();
+        Set<V> vertices = new HashSet<>(), scc;
+
+        for(V v : g.vertices()){
+            if(!vertices.contains(v)){
+                scc = sccOf(g, v);
+                res.add(scc);
+                vertices.addAll(scc);
+            }
+        }
+
+        return res;
+    }
+}
+
